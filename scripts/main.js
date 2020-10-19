fe128bb0a358a5facb377c0b0efe235338f2a77e
Asada678 <55794572+Asada678@users.noreply.github.com>
2020-10-20 01:48:15 +0900

    touch 15

--- scripts/main.js
+++ scripts/main.js
@@ -66,7 +66,7 @@ class Main {
       showActiveTooltip: true,
       controlArrows: false,
       slidesNavigation: true,
-      touchSensitivity: 10,
+      touchSensitivity: 15,
       afterLoad: function (origin, destination, direction) {
         console.log('origin, destination, direction:', origin, destination, direction);
         const destinationSectionContents = document.querySelectorAll(`.section[data-anchor=${destination.anchor}] .content`);
